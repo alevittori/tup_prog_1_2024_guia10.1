a38b8f600ac1c94a9213ce9ee7abbceaf7318002
AleNotebook <alejandrovitt@hotmail.com>
2024-06-15 21:51:48 -0300

    fin del ejercicio

--- Encuestas/Guia10/Form1.Designer.cs
+++ Encuestas/Guia10/Form1.Designer.cs
@@ -29,34 +29,67 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.rbBicicleta = new System.Windows.Forms.RadioButton();
-            this.rbMoto = new System.Windows.Forms.RadioButton();
-            this.rbAuto = new System.Windows.Forms.RadioButton();
+            this.tbDistancia = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.rbTransPublico = new System.Windows.Forms.RadioButton();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rbAuto = new System.Windows.Forms.RadioButton();
+            this.rbMoto = new System.Windows.Forms.RadioButton();
+            this.rbBicicleta = new System.Windows.Forms.RadioButton();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.lBresultados = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnRegistrar);
             this.groupBox1.Controls.Add(this.groupBox3);
             this.groupBox1.Controls.Add(this.groupBox2);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Location = new System.Drawing.Point(17, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(598, 255);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(631, 314);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ingrese número";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(479, 87);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(129, 63);
+            this.btnRegistrar.TabIndex = 2;
+            this.btnRegistrar.Text = "Registrar Encuesta";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tbDistancia);
+            this.groupBox3.Location = new System.Drawing.Point(9, 191);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(415, 111);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Distancia aproximada según el vehiculo seleccionado";
+            // 
+            // tbDistancia
+            // 
+            this.tbDistancia.Location = new System.Drawing.Point(32, 52);
+            this.tbDistancia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbDistancia.Name = "tbDistancia";
+            this.tbDistancia.Size = new System.Drawing.Size(186, 22);
+            this.tbDistancia.TabIndex = 0;
             // 
             // groupBox2
             // 
@@ -64,125 +97,112 @@
             this.groupBox2.Controls.Add(this.rbAuto);
             this.groupBox2.Controls.Add(this.rbMoto);
             this.groupBox2.Controls.Add(this.rbBicicleta);
-            this.groupBox2.Location = new System.Drawing.Point(7, 20);
+            this.groupBox2.Location = new System.Drawing.Point(9, 25);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(413, 129);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(415, 159);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tipo de transporte de uso más frecuente";
             // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Location = new System.Drawing.Point(7, 155);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(413, 90);
-            this.groupBox3.TabIndex = 1;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Distancia aproximada según el vehiculo seleccionado";
-            // 
-            // rbBicicleta
-            // 
-            this.rbBicicleta.AutoSize = true;
-            this.rbBicicleta.Location = new System.Drawing.Point(24, 26);
-            this.rbBicicleta.Name = "rbBicicleta";
-            this.rbBicicleta.Size = new System.Drawing.Size(65, 17);
-            this.rbBicicleta.TabIndex = 0;
-            this.rbBicicleta.TabStop = true;
-            this.rbBicicleta.Text = "Bicicleta";
-            this.rbBicicleta.UseVisualStyleBackColor = true;
-            // 
-            // rbMoto
-            // 
-            this.rbMoto.AutoSize = true;
-            this.rbMoto.Location = new System.Drawing.Point(24, 50);
-            this.rbMoto.Name = "rbMoto";
-            this.rbMoto.Size = new System.Drawing.Size(80, 17);
-            this.rbMoto.TabIndex = 1;
-            this.rbMoto.TabStop = true;
-            this.rbMoto.Text = "Motocicleta";
-            this.rbMoto.UseVisualStyleBackColor = true;
-            // 
-            // rbAuto
-            // 
-            this.rbAuto.AutoSize = true;
-            this.rbAuto.Location = new System.Drawing.Point(24, 74);
-            this.rbAuto.Name = "rbAuto";
-            this.rbAuto.Size = new System.Drawing.Size(71, 17);
-            this.rbAuto.TabIndex = 2;
-            this.rbAuto.TabStop = true;
-            this.rbAuto.Text = "Automovil";
-            this.rbAuto.UseVisualStyleBackColor = true;
-            // 
             // rbTransPublico
             // 
             this.rbTransPublico.AutoSize = true;
-            this.rbTransPublico.Location = new System.Drawing.Point(24, 98);
+            this.rbTransPublico.Location = new System.Drawing.Point(32, 121);
+            this.rbTransPublico.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbTransPublico.Name = "rbTransPublico";
-            this.rbTransPublico.Size = new System.Drawing.Size(216, 17);
+            this.rbTransPublico.Size = new System.Drawing.Size(269, 20);
             this.rbTransPublico.TabIndex = 3;
             this.rbTransPublico.TabStop = true;
             this.rbTransPublico.Text = "Transporte público (colectivo, remis, etc)";
             this.rbTransPublico.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // rbAuto
             // 
-            this.button1.Location = new System.Drawing.Point(445, 70);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 51);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Registrar Encuesta";
-            this.button1.UseVisualStyleBackColor = true;
+            this.rbAuto.AutoSize = true;
+            this.rbAuto.Location = new System.Drawing.Point(32, 91);
+            this.rbAuto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbAuto.Name = "rbAuto";
+            this.rbAuto.Size = new System.Drawing.Size(87, 20);
+            this.rbAuto.TabIndex = 2;
+            this.rbAuto.TabStop = true;
+            this.rbAuto.Text = "Automovil";
+            this.rbAuto.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // rbMoto
             // 
-            this.textBox1.Location = new System.Drawing.Point(89, 39);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(193, 20);
-            this.textBox1.TabIndex = 0;
+            this.rbMoto.AutoSize = true;
+            this.rbMoto.Location = new System.Drawing.Point(32, 62);
+            this.rbMoto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbMoto.Name = "rbMoto";
+            this.rbMoto.Size = new System.Drawing.Size(97, 20);
+            this.rbMoto.TabIndex = 1;
+            this.rbMoto.TabStop = true;
+            this.rbMoto.Text = "Motocicleta";
+            this.rbMoto.UseVisualStyleBackColor = true;
+            // 
+            // rbBicicleta
+            // 
+            this.rbBicicleta.AutoSize = true;
+            this.rbBicicleta.Location = new System.Drawing.Point(32, 32);
+            this.rbBicicleta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbBicicleta.Name = "rbBicicleta";
+            this.rbBicicleta.Size = new System.Drawing.Size(79, 20);
+            this.rbBicicleta.TabIndex = 0;
+            this.rbBicicleta.TabStop = true;
+            this.rbBicicleta.Text = "Bicicleta";
+            this.rbBicicleta.UseVisualStyleBackColor = true;
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.button2);
-            this.groupBox4.Controls.Add(this.listBox1);
-            this.groupBox4.Location = new System.Drawing.Point(13, 269);
+            this.groupBox4.Controls.Add(this.btnResultado);
+            this.groupBox4.Controls.Add(this.lBresultados);
+            this.groupBox4.Location = new System.Drawing.Point(17, 331);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(598, 243);
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Size = new System.Drawing.Size(631, 299);
             this.groupBox4.TabIndex = 1;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Resultados";
             // 
-            // listBox1
+            // btnResultado
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(6, 19);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(414, 212);
-            this.listBox1.TabIndex = 0;
+            this.btnResultado.Location = new System.Drawing.Point(479, 126);
+            this.btnResultado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(129, 66);
+            this.btnResultado.TabIndex = 1;
+            this.btnResultado.Text = "Ver Resultados";
+            this.btnResultado.UseVisualStyleBackColor = true;
+            this.btnResultado.Click += new System.EventHandler(this.btnResultado_Click);
             // 
-            // button2
+            // lBresultados
             // 
-            this.button2.Location = new System.Drawing.Point(445, 106);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 54);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Ver Resultados";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lBresultados.FormattingEnabled = true;
+            this.lBresultados.ItemHeight = 16;
+            this.lBresultados.Location = new System.Drawing.Point(8, 23);
+            this.lBresultados.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lBresultados.Name = "lBresultados";
+            this.lBresultados.Size = new System.Drawing.Size(416, 260);
+            this.lBresultados.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(619, 524);
+            this.ClientSize = new System.Drawing.Size(674, 645);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Encuesta";
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -197,11 +217,11 @@
         private System.Windows.Forms.RadioButton rbAuto;
         private System.Windows.Forms.RadioButton rbMoto;
         private System.Windows.Forms.RadioButton rbBicicleta;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox tbDistancia;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnResultado;
+        private System.Windows.Forms.ListBox lBresultados;
     }
 }
 
--- Encuestas/Guia10/Form1.cs
+++ Encuestas/Guia10/Form1.cs
@@ -12,9 +12,52 @@ namespace Guia10
 {
     public partial class Form1 : Form
     {
+        #region Mis Variables
+        int poblacion = 0, cantBici = 0, cantMoto = 0, cantAuto = 0, cantTp=0;
+
+      
+
+        double distPromedioBici = 0, disPromedioMoto = 0;
+
+        #endregion
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            lBresultados.Items.Clear();
+            lBresultados.Items.Add($"Poblacion: {poblacion.ToString()}");
+            lBresultados.Items.Add("---------------");
+            lBresultados.Items.Add($"-Bicicleta: {cantBici.ToString()}");
+            lBresultados.Items.Add($"-Motocicleta: {cantMoto.ToString()}");
+            lBresultados.Items.Add($"-Automovil: {cantAuto.ToString()}");
+            lBresultados.Items.Add($"-Transporte público: {cantTp.ToString()}");
+            lBresultados.Items.Add("---------------");
+            lBresultados.Items.Add("Distancia promedio por tipo de vehiculo:");
+            lBresultados.Items.Add($"-Bicicleta: {distPromedioBici/cantBici}");
+            lBresultados.Items.Add($"-Motocicleta: {disPromedioMoto/ cantMoto}");
+
+        }
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if(rbBicicleta.Checked) {
+                cantBici++;
+                distPromedioBici += double.Parse(tbDistancia.Text);
+            }
+            if(rbMoto.Checked)
+            {
+                cantMoto++;
+                disPromedioMoto += double.Parse(tbDistancia.Text);
+            }
+            if(rbAuto.Checked) { cantAuto++; }
+            if(rbTransPublico.Checked) { cantTp++; }
+
+            poblacion++;
+
+            tbDistancia.Clear();
+
         }
     }
 }
